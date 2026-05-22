224c3c4a987d4ec80d874da37973ad08d2ab0451
Unixem <Unixemdev@gmail.com>
2026-05-22 15:37:44 -0400

    End of May 22nd Work
    Did some stuff for the projects

--- Assets/LevelsnStuffLol/Game 1/Scripts/PlayerController1.cs
+++ Assets/LevelsnStuffLol/Game 1/Scripts/PlayerController1.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PlayerController1 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+   private Rigidbody rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   void Start()
+   {
+      rb = GetComponent<Rigidbody>();
+   }
+
+   void FixedUpdate()
+   {
+      float moveHorizontal = Input.GetAxis("Horizontal");
+      
+      float moveVertical = Input.GetAxis("Vertical");
+      
+      Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+      rb.AddForce(movement);
+   }
 }
